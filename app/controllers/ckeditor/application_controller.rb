93a7b7b512c527d54f6601d1aad9333afd17f173
Anthony Figueroa <afigueroa@toptierlabs.com>
2013-02-28 12:39:29 -0200

    Iframe compatible

--- app/controllers/ckeditor/application_controller.rb
+++ app/controllers/ckeditor/application_controller.rb
@@ -27,7 +27,7 @@ class Ckeditor::ApplicationController < ::ApplicationController
         render :text => body
       else
         body = %Q"<script type='text/javascript'>
-         if (typeof window.ckeditorCallback == 'function') { window.ckeditorCallback('error');  }
+         if (typeof window.parent.ckeditorCallback == 'function') { window.parent.ckeditorCallback('error');  }
         </script>"
         render :text => body
       end
